00fb1e90af6bf65b0122f384919a227954e7c4c4
anunay_kumar <anunay_kumar@hotmail.com>
2021-07-11 17:18:48 +0530

    Hosting website on github

---
+++ mail/send_mail.php
@@ -0,0 +1,54 @@
+<?php
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "anunay_kumar@hotmail.com";
+
+/*
+This bit sets the URLs of the supporting pages.
+If you change the names of any of the pages, you will need to change the values here.
+*/
+$feedback_page = "feedback_form.html";
+$error_page = "error_message.html";
+$thankyou_page = "thank_you.html";
+
+/*
+This next bit loads the form field data into variables.
+If you add a form field, you will need to add it here.
+*/
+$email_address = $_REQUEST['email_address'] ;
+$comments = $_REQUEST['comments'] ;
+$first_name = $_REQUEST['first_name'] ;
+$msg = 
+"First Name: " . $first_name . "\r\n" . 
+"Email: " . $email_address . "\r\n" . 
+"Comments: " . $comments ;
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+	mail( "$webmaster_email", "Feedback Form Results", $msg );
+
+
+?>
